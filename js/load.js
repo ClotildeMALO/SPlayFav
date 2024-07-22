21e6b5f9dc69bb8ae2d22b66f752002776b7235d
ClotildeMALO <clotilde.malo@iut-dijon.u-bourgogne.fr>
2024-07-22 18:16:47 +0200

    Ajout bouton goTop

--- js/load.js
+++ js/load.js
@@ -29,3 +29,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     exportAllLikedTracks();
 });
+
+
+document.querySelector('#up')
+	.addEventListener('click', function (e) {
+  	window.scrollTo({
+        top: 0,
+        left:0,
+        behavior: 'smooth'
+    });
+  })
